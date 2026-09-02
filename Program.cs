53eebeb712eff334ca07bedcace7db00fd0696d1
24030 <24030@WS-2902>
2026-09-02 12:29:11 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using PlacesLibrary;
+
+namespace _10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
